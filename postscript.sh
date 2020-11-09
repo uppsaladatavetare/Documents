40237ab3f14e77059f75862f07fafd564c3c66eb
Mojken <mikaelgillis@gmail.com>
2020-11-09 09:24:16 +0100

    Add .tex files and CI (#1)
    * Initial Commit
    
    * Automatically generated
    
    * Change names of steps in CI
    
    And the name of the workflow itself.
    
    * Remove artifact
    
    Probably shouldn't be there, it was convenient for bugtesting
    
    * Automatically generated
    
    Co-authored-by: Uppsala Datavetare <dv-ordf@utn.se>

---
+++ postscript.sh
@@ -0,0 +1,17 @@
+# Fixes some graphical bugs with the transparent logo.
+
+gs -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite \
+-dAutoFilterColorImages=true \
+-dAutoFilterGrayImages=true \
+-dDownsampleMonoImages=true \
+-dDownsampleGrayImages=true \
+-dDownsampleColorImages=true \
+-sOutputFile=docs/UD-reglemente.pdf UD-reglemente.pdf 
+
+gs -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite \
+-dAutoFilterColorImages=true \
+-dAutoFilterGrayImages=true \
+-dDownsampleMonoImages=true \
+-dDownsampleGrayImages=true \
+-dDownsampleColorImages=true \
+-sOutputFile=docs/UD-stadgar.pdf UD-stadgar.pdf
