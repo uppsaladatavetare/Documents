1502960198c3a69247109879a34dff151ae4aaf1
dv-sekreterare <dv-sekreterare@utn.se>
2022-02-16 11:39:39 +0100

    Checkers intitial file

---
+++ Downloads/emacs-27.2-x86_64/bin/Checkers.hs
@@ -0,0 +1,21 @@
+data Square = undefined
+data Checkers = undefined
+type Board = [Square, Checkers]
+type Move = (Square, Square)
+
+
+
+moveChecker = undefined
+
+captureChecker = undefined
+
+printBoard = undefined
+
+victory :: Board -> Bool
+victory = undefined
+
+{-
+-}
+main = undefined
+
+play = undefined
